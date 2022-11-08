533bfae2fe8728a4fbd7b75a083d5c70119582fe
mj0717 <a81930875@gmail.com>
2022-11-08 17:14:45 +0900

    6

--- main.c
+++ main.c
@@ -4,23 +4,16 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	FILE *fp;
-	char str[30];
-	fp=fopen("sample.txt","w");
+	FILE *fp=NULL;
+	char c;
 	
+	fp=fopen("sample.txt","r");
 	
-	printf("input a word: ");
-	scanf("%s",str);
-	fprintf(fp,"%s\n",str);
-	
-	printf("input a word: ");
-	scanf("%s",str);
-	fprintf(fp,"%s\n",str);
-	
-	printf("input a word: ");
-	scanf("%s",str);
-	fprintf(fp,"%s\n",str);
-	
+	c=fgetc(fp);//글자 하나 가져옴 
+	while((c=fgetc(fp))!=EOF)
+		putchar(c);
+		
+		
 	fclose(fp);
 
 	
