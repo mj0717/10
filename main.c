4842608d45d9716ac8f3f4d6a3356ac180987f96
mj0717 <a81930875@gmail.com>
2022-11-08 16:48:27 +0900

    4

--- main.c
+++ main.c
@@ -1,17 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char *argv[]) {
-	int i;
-	char str[4];
-	str[0]='a';
-	str[1]='b';
-	str[2]='c';
-	str[3]='\0';
+
+void main(void){
+	char str[30]="happy C programming";
 	
-	printf("%s\n",str);
 	
-	return 0;
+	printf("문자열 \"%s\"의 길이: %i", str,strlen(str));
 }
